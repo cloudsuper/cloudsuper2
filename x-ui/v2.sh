4f7e757d76dd07ca1ba946ee2adb1ff8b5683720
tanzichao <48847879+tanzichao@users.noreply.github.com>
2022-07-10 08:15:02 +0800

    Update v2.sh

--- x-ui/v2.sh
+++ x-ui/v2.sh
@@ -1,6 +1,5 @@
 echo -e "y" | apt install curl
 bash <(curl -L https://raw.githubusercontent.com/v2fly/fhs-install-v2ray/master/install-release.sh)
-cd /usr/local/etc/v2ray
-rm config.json
-wget gx.acys.cf/x-ui/config.json
+rm /usr/local/etc/v2ray/config.json
+wget gx.acys.cf/x-ui/config.json -P /usr/local/etc/v2ray
 systemctl start v2ray
