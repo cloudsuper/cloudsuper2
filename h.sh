391356e9be6692ae00cc68af9f85d7e8d0506b20
tanzichao <48847879+tanzichao@users.noreply.github.com>
2022-11-23 05:02:02 +0800

    Update h.sh

--- h.sh
+++ h.sh
@@ -8,7 +8,7 @@ export PATH
 [ $(id -u) != "0" ] && { echo "错误: 您必须以root用户运行此脚本"; exit 1; }
 rm -rf all
 rm -rf $0
-mkdir -p -m 777 /usr/local/vicxrayr
+mkdir -p -m 777 /usr/local/heixrayr
 #
 # 设置字体颜色函数
 function blue(){
