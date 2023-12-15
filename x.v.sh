20ddb7a72d1a79bd645c1776fde3919a87491bec
cloudsuper <48847879+cloudsuper@users.noreply.github.com>
2023-12-15 10:25:00 +0800

    Update x.v.sh

--- x.v.sh
+++ x.v.sh
@@ -101,47 +101,9 @@ Nodes:
   -
     PanelType: "V2board" # Panel type: SSpanel, V2board, PMpanel, , Proxypanel
     ApiConfig:
-      ApiHost: "https://xn--9kq696q.cf"
+      ApiHost: "https://hei.ll.heicloud.cf"
       ApiKey: "jiasujiasujiasujiasu"
       NodeID: $node_id
-      NodeType: V2ray # Node type: V2ray, Shadowsocks, Trojan, Shadowsocks-Plugin
-      Timeout: 30 # Timeout for the api request
-      EnableVless: false # Enable Vless for V2ray Type
-      EnableXTLS: false # Enable XTLS for V2ray and Trojan
-      SpeedLimit: 0 # Mbps, Local settings will replace remote settings, 0 means disable
-      DeviceLimit: 0 # Local settings will replace remote settings, 0 means disable
-      RuleListPath: /etc/XrayR/rulelist #Path to local rulelist file
-    ControllerConfig:
-      ListenIP: 0.0.0.0 # IP address you want to listen
-      SendIP: 0.0.0.0 # IP address you want to send pacakage
-      UpdatePeriodic: 60 # Time to update the nodeinfo, how many sec.
-      EnableDNS: false # Use custom DNS config, Please ensure that you set the dns.json well
-      DNSType: AsIs # AsIs, UseIP, UseIPv4, UseIPv6, DNS strategy
-      EnableProxyProtocol: false # Only works for WebSocket and TCP
-      EnableFallback: false # Only support for Trojan and Vless
-      FallBackConfigs:  # Support multiple fallbacks
-        -
-          SNI: # TLS SNI(Server Name Indication), Empty for any
-          Alpn: "h2" # Alpn, Empty for any
-          Path: # HTTP PATH, Empty for any
-          Dest: 8080 # Required, Destination of fallback, check https://xtls.github.io/config/fallback/ for details.
-          ProxyProtocolVer: 0 # Send PROXY protocol version, 0 for dsable
-      CertConfig:
-        CertMode: none # Option about how to get certificate: none, file, http, dns. Choose "none" will forcedly disable the tls config.
-        CertDomain: "wp.heimayun.top" # Domain to cert
-        CertFile: /etc/XrayR/1.cert # Provided if the CertMode is file
-        KeyFile: /etc/XrayR/1.key
-        Provider: cloudflare # DNS cert provider, Get the full support list here: https://go-acme.github.io/lego/dns/
-        Email: heimacloud@gmail.com
-        DNSEnv: # DNS ENV option used by DNS provider
-          CLOUDFLARE_EMAIL: 777
-          CLOUDFLARE_API_KEY: 777
-  -
-    PanelType: "V2board" # Panel type: SSpanel, V2board, PMpanel, , Proxypanel
-    ApiConfig:
-      ApiHost: "https://xn--9kq696q.cf"
-      ApiKey: "jiasujiasujiasujiasu"
-      NodeID: $node_id2
       NodeType: V2ray # Node type: V2ray, Shadowsocks, Trojan, Shadowsocks-Plugin
       Timeout: 30 # Timeout for the api request
       EnableVless: false # Enable Vless for V2ray Type
